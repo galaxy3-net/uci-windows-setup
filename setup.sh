68387424c88ec50fa2490134880f983287dfe788
Korben Kirscht <kirscht@gmail.com>
2021-02-11 18:09:21 -0800

    Creating setup.ps1

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stat ~/galaxy3 || git clone -b thedr https://github.com/galaxy3-net/galaxy3.git ~/galaxy3
+stat ~/galaxy3 2>/dev/null || git clone -b thedr https://github.com/galaxy3-net/galaxy3.git ~/galaxy3
 
 export SOURCE="source ~/galaxy3/env"
 
