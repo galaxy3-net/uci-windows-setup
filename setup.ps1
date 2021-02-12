d14152aa509ae89cc9c013e6bdb1891de641d58e
Korben Kirscht <kirscht@gmail.com>
2021-02-11 17:31:36 -0800

    Creating setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -2,3 +2,6 @@ Write-Output 'Setup for UCI Cybersecurity Bootcamp'
 Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco install -y VisualStudioCode
 choco install -y git.install
+choco install -y python3
+choco install -y virtualbox
+choco install -y vagrant
