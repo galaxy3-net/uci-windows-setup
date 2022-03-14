08b89b54e358ba777c3849341b89a6910904ceb8
korbenkirscht <korben@kirscht.com>
2022-03-13 22:44:51 -0700

    Adding uciuser

--- setup.ps1
+++ setup.ps1
@@ -23,11 +23,25 @@ else
 {
     Write-Debug "Copying $RTSZFile"
     # $zipFile = "https://drive.google.com/uc?export=download&id=1S-5U8eSWxNzXSNftmVctrUD6fRl4oVb3"
-    $zipFile =
+    $zipFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/files/UCI-Cybersecurity.rtsz"
+    Invoke-WebRequest -Uri $zipFile -OutFile $RTSZFile
+}
+
+$RTSZFile = "$HOME\Documents\Azure Labs.rtsz"
+if (Test-Path $RTSZFile)
+{
+    Write-Debug "$RTSZFile Exists"
+}
+else
+{
+    Write-Debug "Copying $RTSZFile"
+    # $zipFile = "https://drive.google.com/uc?export=download&id=1S-5U8eSWxNzXSNftmVctrUD6fRl4oVb3"
+    $zipFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/files/Azure Labs.rtsz"
     Invoke-WebRequest -Uri $zipFile -OutFile $RTSZFile
 }
 
 #$zipFile = "https://drive.google.com/uc?export=download&id=1VCwMDZxSi-Yv-kyAYetYPWDkF7FYLQM4"
+$zipFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/files/UCI-Cybersecurity.html"
 Invoke-WebRequest -Uri $zipFile -OutFile "$HOME\Documents\UCI-Cybersecurity.html"
 
 # https://drive.google.com/file/d/1S-5U8eSWxNzXSNftmVctrUD6fRl4oVb3/view?usp=sharing
