d9fc2b5937a609490251878ca0828cba6846c4d1
korbenkirscht <korben@kirscht.com>
2022-09-25 20:07:08 -0700

    New scripts

--- New-User.ps1
+++ New-User.ps1
@@ -7,7 +7,7 @@ New-LocalUser -name $MyUser -Password $PASSWORD -ErrorAction Inquire ; Write-Hos
 
 Add-LocalGroupMember -Group "Administrators" -Member $MyUser -ErrorAction Inquire ; Write-Host " continuing . . ."
 
-$SourceFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/files/Phase-I.ps1"
+$SourceFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/Phase-I.ps1"
 iwr $SourceFile -OutFile "C:\Users\Public\Desktop\Phase-I.ps1"
 
 $SourceFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/files/UCI-Cybersecurity.html"
