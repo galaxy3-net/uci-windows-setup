4e7a2f57f0a488a3f011e0b5c3b9eb962c90e01e
korbenkirscht <korben@kirscht.com>
2022-09-25 19:24:27 -0700

    New scripts

--- New-User.ps1
+++ New-User.ps1
@@ -3,8 +3,10 @@ param(
     [String]$MyUser = "uciuser"
 )
 $PASSWORD = Read-Host -Prompt "Enter Password for $MyUser" -AsSecureString
-New-LocalUser -name $MyUser -Password $PASSWORD
+New-LocalUser -name $MyUser -Password $PASSWORD -ErrorAction Continue ; Write-Host "$MyUser already exists, continuing"
+
 Add-LocalGroupMember -Group "Administrators" -Member $MyUser
+
 
 $SourceFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/files/Phase-I.ps1"
 iwr -Uri $SourceFile -OutFile "C:\Users\Public\Desktop\Phase-I.ps1"
