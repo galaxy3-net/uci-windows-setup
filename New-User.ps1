97d3aa88af2d20a84778985d62d781467835e934
korbenkirscht <korben@kirscht.com>
2022-09-25 19:41:28 -0700

    New scripts

--- New-User.ps1
+++ New-User.ps1
@@ -5,7 +5,7 @@ param(
 $PASSWORD = Read-Host -Prompt "Enter Password for $MyUser" -AsSecureString
 New-LocalUser -name $MyUser -Password $PASSWORD -ErrorAction Inquire ; Write-Host " continuing . . ."
 
-Add-LocalGroupMember -Group "Administrators" -Member $MyUserD -ErrorAction Inquire ; Write-Host " continuing . . ."
+Add-LocalGroupMember -Group "Administrators" -Member $MyUser -ErrorAction Inquire ; Write-Host " continuing . . ."
 
 $SourceFile = "https://raw.githubusercontent.com/galaxy3-net/uci-windows-setup/dev/files/Phase-I.ps1"
 iwr -Uri $SourceFile -OutFile "C:\Users\Public\Desktop\Phase-I.ps1"
